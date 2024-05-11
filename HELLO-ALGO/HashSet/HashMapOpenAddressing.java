6616089d3d24caaa4a6de0805a89e0720f43cff7
SkyDependence <nanyangzesi@gmail.com>
2024-05-11 10:47:03 +0800

    change file name and write all the function which will be finished.

---
+++ HELLO-ALGO/HashSet/HashMapOpenAddressing.java
@@ -0,0 +1,66 @@
+// 键值对
+
+
+// 基于数组实现的哈希表
+class OpenAddressingHashMap {
+
+    // 构造方法
+    public OpenAddressingHashMap() {
+
+    }
+
+    // 哈希函数
+    public int hashFunc(int key) {
+
+    }
+
+    // 负载因子
+    public double loadFactor() {
+
+    }
+
+    // 查询操作
+    public String get(int key) {
+
+    } 
+
+    // 添加
+    public void put(int key, String value) {
+
+    }
+
+    // 删除
+    public void remove(int key) {
+
+    }
+
+    // 扩容哈希表
+    public void extend() {
+
+    }
+
+    // 获取所有键
+    public List<Integer> keySet() {
+
+    }
+
+    // 获取所有键值对
+    public List<Pair> pairSet() {
+
+    }
+
+    // 获取所有值
+    public List<String> valueSet() {
+
+    }
+
+    // 打印哈希表
+    public void print() {
+
+    }
+}
+
+
+public class HashMapOpenAddressing {
+    
+}
