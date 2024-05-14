f0a518b8e55be6238b6dd266338ad85378c383fb
SkyDependence <nanyangzesi@gmail.com>
2024-05-14 10:41:24 +0800

    complete the structure of ArraysTree

---
+++ HELLO-ALGO/tree/ArraysTree.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArraysTree {
+    private List<Integer> tree;
+
+    // 构造方法
+    public ArraysTree(List<Integer> list) {
+        tree = new ArrayList<>(list);
+    }
+
+    // 列表容量
+    public int size() {
+        return tree.size();
+    }
+
+    // 获取索引为 i 节点的值
+    public Integer val(int i) {
+
+    }
+
+    // 获取索引为 i 节点的左子节点的索引
+    public Integer left(int i) {
+
+    }
+
+    // 获取索引为 i 节点的右子节点的索引
+    public Integer right(int i) {
+
+    }
+
+    // 层序遍历
+    public List<Integer> levelOrder() {
+
+    }
+
+    // 深度优先遍历
+    public void dfs(Integer i, String order, List<Integer> res) {
+
+    }
+
+    // 前序遍历
+    public List<Integer> preOrder() {
+
+    }
+
+    // 中序遍历
+    public List<Integer> inOrder() {
+
+    }
+
+    // 后序遍历
+    public List<Integer> postOrder() {
+
+    }
+    
+}
