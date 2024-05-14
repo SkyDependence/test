8a3f6527a0962e1094f5f220a9cf663210488d59
SkyDependence <nanyangzesi@gmail.com>
2024-05-14 20:44:36 +0800

    complete the ArraysTree

--- HELLO-ALGO/tree/ArraysTree.java
+++ HELLO-ALGO/tree/ArraysTree.java
@@ -16,42 +16,67 @@ public class ArraysTree {
 
     // 获取索引为 i 节点的值
     public Integer val(int i) {
-
+        if (i < 0 || i >= size()) {
+            return null;
+        }
+        return tree.get(i);
     }
 
     // 获取索引为 i 节点的左子节点的索引
     public Integer left(int i) {
-
+        return i * 2 + 1;
     }
 
     // 获取索引为 i 节点的右子节点的索引
     public Integer right(int i) {
-
+        return i * 2 + 2;
     }
 
     // 层序遍历
     public List<Integer> levelOrder() {
-
+        List<Integer> res = new ArrayList<>();
+        for (int i : tree) {
+            res.add(i);
+        }
+        return res;
     }
 
     // 深度优先遍历
     public void dfs(Integer i, String order, List<Integer> res) {
-
+        if (val(i) == null) {
+            return;
+        }
+        if (order.equals("pre")) {
+            res.add(val(i));
+        }
+        dfs(left(i), order, res);
+        if (order.equals("in")) {
+            res.add(val(i));
+        }
+        dfs(right(i), order, res);
+        if (order.equals("post")) {
+            res.add(val(i));
+        }
     }
 
     // 前序遍历
     public List<Integer> preOrder() {
-
+        List<Integer> res = new ArrayList<>();
+        dfs(0, "pre", res);
+        return res;
     }
 
     // 中序遍历
     public List<Integer> inOrder() {
-
+        List<Integer> res = new ArrayList<>();
+        dfs(0, "in", res);
+        return res;
     }
 
     // 后序遍历
     public List<Integer> postOrder() {
-
+        List<Integer> res = new ArrayList<>();
+        dfs(0, "post", res);
+        return res;
     }
-    
 }
--- HELLO-ALGO/tree/TreeTest.java
+++ HELLO-ALGO/tree/TreeTest.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class TreeTest {
     public static void main(String[] args) {
@@ -15,12 +15,32 @@ public class TreeTest {
         n2.right = n5;
 
         // BFS
-        System.out.println(root.levelOrder());
-        System.out.println("/******************************/");
-        System.out.println(root.preOrderRes());
-        System.out.println("/******************************/");
-        System.out.println(root.inOrderRes());
-        System.out.println("/******************************/");
-        System.out.println(root.postOrderRes());
+        // System.out.println(root.levelOrder());
+        // System.out.println("/******************************/");
+        // System.out.println(root.preOrderRes());
+        // System.out.println("/******************************/");
+        // System.out.println(root.inOrderRes());
+        // System.out.println("/******************************/");
+        // System.out.println(root.postOrderRes());
+
+        // 创建一个示例数组树
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        ArraysTree tree = new ArraysTree(list);
+
+        // 测试层序遍历
+        List<Integer> levelOrderResult = tree.levelOrder();
+        System.out.println("层序遍历结果: " + levelOrderResult);
+
+        // 测试前序遍历
+        List<Integer> preOrderResult = tree.preOrder();
+        System.out.println("前序遍历结果: " + preOrderResult);
+
+        // 测试中序遍历
+        List<Integer> inOrderResult = tree.inOrder();
+        System.out.println("中序遍历结果: " + inOrderResult);
+
+        // 测试后序遍历
+        List<Integer> postOrderResult = tree.postOrder();
+        System.out.println("后序遍历结果: " + postOrderResult);
     }
 }
